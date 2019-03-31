15d2474b93c903803c77e9bc9031bbd916fb7c18
Trace Larson <trace.larson4917@gmail.com>
2019-03-31 16:20:52 -0500

    refreshing props and propTypes

--- lmrma/src/App.js
+++ lmrma/src/App.js
@@ -1,51 +1,37 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
-const welcome = "welcome to react";
+import Movie from "./Movie";
+
+const movies = [
+  {
+    id: 1,
+    title: "Star Wars",
+    desc: "A galaxy far far away"
+  },
+  {
+    id: 2,
+    title: "Spider-Man"
+  },
+  {
+    id: 3,
+    title: "Kung-foo Hustle"
+  }
+];
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor");
-  }
-
-  componentWillMount() {
-    console.log("will mount");
-  }
-
-  componentDidMount() {
-    console.log("mounted");
-  }
-
-  state = {
-    toggle: true
-  };
-
-  handleToggle = () => {
-    this.setState({
-      toggle: !this.state.toggle
-    });
-  };
-
   render() {
-    const { toggle } = this.state;
-    console.log(toggle);
     return (
       <div className="App">
         <header className="App-header">
-          <Welcome text={welcome} toggle={toggle} />
-          {toggle && <p>This should show and hide</p>}
-          <button onClick={this.handleToggle}>Show / Hide</button>
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} desc={movie.desc} />
+        ))}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text, toggle } = this.props;
-    return <h1>{toggle && text}</h1>;
   }
 }
 
---
+++ lmrma/src/Movie.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }),
+    desc: PropTypes.string
+  };
+
+  static defaultProps = {
+    desc: "Description not available"
+  };
+
+  render() {
+    const { movie, desc } = this.props;
+    return (
+      <div>
+        <h3>{movie.title}</h3>
+        <p>{desc}</p>
+      </div>
+    );
+  }
+}
