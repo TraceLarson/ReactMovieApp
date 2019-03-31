e587b7e4c5c2df91de89f0334d74ec15c897a763
Trace Larson <trace.larson4917@gmail.com>
2019-03-30 21:53:52 -0500

    refreshing on life cycle methods, props, and state

--- lmrma/src/App.js
+++ lmrma/src/App.js
@@ -1,27 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const welcome = "welcome to react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  componentWillMount() {
+    console.log("will mount");
+  }
+
+  componentDidMount() {
+    console.log("mounted");
+  }
+
+  state = {
+    toggle: true
+  };
+
+  handleToggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    });
+  };
+
   render() {
+    const { toggle } = this.state;
+    console.log(toggle);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Welcome text={welcome} toggle={toggle} />
+          {toggle && <p>This should show and hide</p>}
+          <button onClick={this.handleToggle}>Show / Hide</button>
         </header>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text, toggle } = this.props;
+    return <h1>{toggle && text}</h1>;
   }
 }
 
